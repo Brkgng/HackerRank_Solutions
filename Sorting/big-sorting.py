f9b48cf92193df3184b34518730c79458ed3c8cd
Berker <berkergng457@gmail.com>
2021-07-12 23:11:32 +0300

    big-sorting added

---
+++ Sorting/big-sorting.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# See the problem
+# https://www.hackerrank.com/challenges/big-sorting/problem
+#
+#
+
+
+def bigSorting(unsorted):
+    unsorted.sort(key=lambda digits: (len(digits), digits))
+    return unsorted
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
